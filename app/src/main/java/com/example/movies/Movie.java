3cdba043ce12c2de60eac98885658a1efc1b964a
Zyckros <zyckros@gmail.com>
2020-10-28 04:00:59 +0000

    complete request to get JSON and create ArrayList with movies data

---
+++ app/src/main/java/com/example/movies/Movie.java
@@ -0,0 +1,77 @@
+package com.example.movies;
+
+public class Movie {
+
+    private int id;
+    private String title;
+    private Double popularity;
+    private int vote_count;
+    private String poster_path;
+    private String originalTitle;
+    private String overView;
+    private String releaseDate;
+    private Double voteAverage;
+
+    public Movie(int id, String title, String overView, Double popularity, int vote_count, String poster_path, String originalTitle, String releaseDate, Double voteAverage) {
+        this.id = id;
+        this.title = title;
+        this.popularity = popularity;
+        this.vote_count = vote_count;
+        this.poster_path = poster_path;
+        this.originalTitle = originalTitle;
+        this.overView = overView;
+        this.releaseDate = releaseDate;
+        this.voteAverage = voteAverage;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Double getPopularity() {
+        return popularity;
+    }
+
+    public int getVote_count() {
+        return vote_count;
+    }
+
+    public String getPoster_path() {
+        return poster_path;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public String getOverView() {
+        return overView;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", popularity=" + popularity +
+                ", vote_count=" + vote_count +
+                ", poster_path='" + poster_path + '\'' +
+                ", originalTitle='" + originalTitle + '\'' +
+                ", overView='" + overView + '\'' +
+                ", releaseDate='" + releaseDate + '\'' +
+                ", voteAverage=" + voteAverage +
+                '}';
+    }
+}
--- app/src/main/java/com/example/movies/MoviesListActivity.java
+++ app/src/main/java/com/example/movies/MoviesListActivity.java
@@ -3,12 +3,122 @@ package com.example.movies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 
 public class MoviesListActivity extends AppCompatActivity {
+
+    //https://api.themoviedb.org/3/movie/550?api_key=bf609558decf0378637cd30a5f0e04f2
+
+    TextView textViewTitleMovie;
+    ArrayList<Movie> movies;
+    private RequestQueue mQeue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_movies_list);
+
+
+        getMovies();
+
     }
+
+    /**
+     * This function make a request to api.themoviedb.org and get JSON Movies
+     */
+    private void getMovies() {
+
+         movies = new ArrayList<Movie>();
+        String url = "https://api.themoviedb.org/3/search/movie?api_key=bf609558decf0378637cd30a5f0e04f2&language=en-US&query=1&page=1&include_adult=false";
+        mQeue = Volley.newRequestQueue(this);
+        textViewTitleMovie = findViewById(R.id.textView);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("results");
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject movie = jsonArray.getJSONObject(i);
+                                movies.add(new Movie(movie.getInt("id"), movie.getString("title"), movie.getString("overview"), movie.getDouble("popularity"), movie.getInt("vote_count"), movie.getString("poster_path"), movie.getString("original_title"), movie.getString("release_date"), movie.getDouble("vote_average")));
+                            }
+
+                            for(Movie movie: movies){
+                                textViewTitleMovie.append(movie.toString());
+                            }
+
+                        } catch (Exception e) {
+                            Log.e("JSON", "Exception inside jsonObjectRequest: " + e);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.v("JSON", "Fallo comunicacion" + error);
+                    }
+                });
+        mQeue.add(jsonObjectRequest);
+    }
+
 }
+
+
+
+
+/*
+        final TextView textView = findViewById(R.id.textView_title_movie);
+        final ListView listView = findViewById(R.id.list);
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://api.themoviedb.org/3/search/movie?api_key=bf609558decf0378637cd30a5f0e04f2&language=en-US&query=1&page=1&include_adult=false";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                             JSONObject jsonObject = new JSONObject(response);
+                             JSONArray jsonArray = new JSONArray(jsonObject.getJSONArray("results"));
+
+                             //textView.setText(jsonArray.toString());
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                }, new Response.ErrorListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText(error.toString());
+            }
+        });
+
+
+        queue.add(stringRequest);
+*/
